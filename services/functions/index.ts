a0ad8150d86b53880ab86bd11872895c07bb9361
Betarost <betarost@yandex.ru>
2023-12-27 14:22:22 +0300

    .

--- services/functions/index.ts
+++ services/functions/index.ts
@@ -1,4 +1,17 @@
+import { v4 as uuidv4 } from 'uuid';
 
-export const loader = async function (Variable: any) {
+export const loader = async function (Variable: any, Fn: any) {
+  if (!localStorage.uuid) {
+    localStorage.uuid = uuidv4()
+  }
+  let eventSource = new EventSource(`/api/events/MyInfo?uuid=${localStorage.uuid}`)
+  eventSource.addEventListener('update', (answ) => {
+    console.log('=5dcbec=', answ)
+    if (!answ.data || answ.data == "null") {
+      return
+    }
+    let myInfo = JSON.parse(answ.data)
+    Variable.myInfo = Object.assign(Variable.myInfo, myInfo)
+  });
   return
 }
