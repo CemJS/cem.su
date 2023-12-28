119141559e6b1d0af469eb3afe92ad767c61f952
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-12-28 12:35:13 +0300

    .

---
+++ frontends/courseLine/display/Main.tsx
@@ -0,0 +1,25 @@
+import { Cemjsx, Fn } from "cemjs-all"
+import courses from '@json/course'
+
+export default function () {
+    return (
+        <div class="course-line" onclick={Fn.linkChange("/exchange-rates")}>
+            <div class="course-line__track">
+                {
+                    courses.map(item => {
+                        return (
+                            <div class="course-line__item">
+                                <div class="course-line__item-img">
+                                    <img src={`/assets/svg/coins/${item.nameCoin}.svg`} alt={item.nameCoin} />
+                                </div>
+                                <span class="course-line__item-text">{item.nameCoin}</span>
+                                <span>{item.currentCourse}</span>
+
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
