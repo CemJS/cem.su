650e9ea38e580fec215692ddeeb9a7b6454aae18
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2024-01-08 17:23:07 +0300

    .

--- frontends/news/display/Main.tsx
+++ frontends/news/display/Main.tsx
@@ -1,16 +1,18 @@
 import { Cemjsx, Static, Fn } from "cemjs-all"
-import news from "@json/news"
 
 export default function () {
     Fn.log('=3aa4e9=', Static.records)
     return (
         <section class="news">
             {
-                news.map(item => {
+                Static.records.map(item => {
                     return (
                         <div class="news__item">
                             <div class="news__item-img">
-                                <img src={item.img} alt={item.title} />
+                                <img
+                                    src={`https://crypto-emergency.com/assets/upload/news/${item.image}`}
+                                    alt={item.title}
+                                />
                             </div>
                             <h3 class="news__item-title">{item.title}</h3>
                             <p class="news__item-desc">{item.text}</p>
