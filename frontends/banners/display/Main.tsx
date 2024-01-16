b4f0a8a016988f4aaabbb5cd587a0f0b74ff159c
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2024-01-16 17:50:15 +0300

    add minimal for show banners

--- frontends/banners/display/Main.tsx
+++ frontends/banners/display/Main.tsx
@@ -6,7 +6,15 @@ export default function () {
     Fn.log("Static.records banners...", Static.records)
     return (
         <div class="banners">
-
+            {
+                Static.records.map(item => {
+                    return (
+                        <div class="banners__item">
+                            <img src={`/assets/upload/worldPress/${item.name}`} alt="" />
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
