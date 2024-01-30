7729fcebb97b0697dbbf5a22d5686441fbd04f4c
quondem <86314992+quondem@users.noreply.github.com>
2024-01-30 17:55:46 +0300

    ico start

--- frontends/ico/display/Main.tsx
+++ frontends/ico/display/Main.tsx
@@ -1,7 +1,17 @@
-import { Cemjsx } from "cemjs-all"
+import { Cemjsx, Fn, Static } from "cemjs-all";
+import tags from "json/icoTags.json";
 
 export default function () {
-    return (
-        <div>Empty</div>
-    )
+  //   Fn.log("records", Static.records);
+  return (
+    <div class="wrapper">
+      <div class="ico">
+        <div class="ico__tags">
+          {tags.map((item) => {
+            return <div class="ico__tag">{item.text}</div>;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
