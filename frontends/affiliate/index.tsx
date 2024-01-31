eadd6e9092acddc174566800476440cfe85ab920
quondem <86314992+quondem@users.noreply.github.com>
2024-01-31 17:29:40 +0300

    add banners in affiliate

--- frontends/affiliate/index.tsx
+++ frontends/affiliate/index.tsx
@@ -1,25 +1,46 @@
-import { Cemjsx, front, Func, Static, Fn } from "cemjs-all"
-import Navigation from "./navigation"
-
+import { Cemjsx, front, Func, Static, Fn } from "cemjs-all";
+import Navigation from "./navigation";
+import banners from "json/affiliateBanners.json";
 
 front.listener.finish = () => {
-    return
-}
+  return;
+};
 
 front.func.test = () => {
-    return
-}
+  return;
+};
+
+front.func.changeLanguage = () => {
+  if (Static.language == "Русский") {
+    Static.sizes = banners.filter((item) => item.language == "Русский")[0];
+  } else {
+    Static.sizes = banners.filter((item) => item.language == "English")[0];
+  }
+  Static.currentSize = Static.sizes.banners[0];
+  Func.changeLink();
+  return;
+};
+
+front.func.changeLink = () => {
+  Static.addLink = `${Static.currentSize}${Static.language == "Русский" ? "ru" : ""}.jpg`;
+  Static.fullLink = `${window.location.origin}/contents/images/affiliate_banners/${Static.addLink}`;
+  Static.tagLink = `<img src="${Static.fullLink}" alt="Crypto Emergency"/>`;
+};
 
 front.loader = () => {
-    return
-}
+  Static.language = "Русский";
+  Func.changeLanguage();
+  Static.currentSize = Static.sizes.banners[0];
+  Func.changeLink();
+  return;
+};
 
 front.display = () => {
-    return (
-        <div>
-            <Navigation />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+};
 
-export { front }
+export { front };
