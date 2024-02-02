b49b2c391535b099265b36efc41e739702984e24
Konstantin <s4etmc@yandex.ru>
2024-02-02 17:23:47 +0300

    stream

--- frontends/header/navigation.tsx
+++ frontends/header/navigation.tsx
@@ -2,7 +2,7 @@ import { Cemjsx, front, Fn } from "cemjs-all";
 import Main from "./display/Main";
 
 export default function () {
-  Fn.log("=273fa1=", front.Variable.hideHeader);
+  // Fn.log("=273fa1=", front.Variable.hideHeader);
   if (!front.Variable.hideHeader) {
     return <Main />;
   }
