b00236951a1945e5fe91b02f7ed7090bbb54deaa
quondem <86314992+quondem@users.noreply.github.com>
2024-02-19 17:56:12 +0300

    forum fixes

--- frontends/forum/display/Main.tsx
+++ frontends/forum/display/Main.tsx
@@ -9,8 +9,10 @@ import innovation from "@svg/forum/innovation.svg";
 import inspiration from "@svg/forum/inspiration.svg";
 import education from "@svg/forum/education.svg";
 import crypto_emergency from "@svg/forum/crypto_emergency.svg";
-import speakers from "@json/forumSpeakers";
-import partners from "@json/forumPartners";
+import speakers from "@json/forum/forumSpeakers";
+import partners from "@json/forum/forumPartners";
+import guests from "@json/forum/forumGuests";
+import images from "@json/forum/forumImages";
 import introImg from "@images/forum/Crypto.png";
 import metis from "@images/forum/metis.png";
 import racib from "@images/forum/racib.png";
@@ -18,6 +20,7 @@ import schedule3 from "@images/forum/schedule3.png";
 import schedule4 from "@images/forum/schedule4.png";
 
 import { Display } from "@elements/ForumSlider";
+import { DisplayImages } from "@elements/ImagesForumSlider";
 
 const tabs = [
   {
@@ -77,12 +80,10 @@ const contentAbout = [
   },
 ];
 
-Static.partners = partners;
-
 export default function () {
   return (
     <div class="forum">
-      <div class="forum__intro">
+      <section class="forum__intro">
         <div class="wrapper">
           <div class="forum__intro-card">
             <div class="forum__intro-img">
@@ -101,8 +102,8 @@ export default function () {
             </div>
           </div>
         </div>
-      </div>
-      <div class="forum__info forum__info_margin">
+      </section>
+      <section class="forum__info forum__info_margin">
         <div class="wrapper wrapper_padding">
           <div class="forum__info-wrapper">
             <div class="forum__info-invite">
@@ -149,9 +150,9 @@ export default function () {
             </div>
           </div>
         </div>
-      </div>
+      </section>
       <div class="wrapper wrapper_padding">
-        <div class="forum__opportunities">
+        <section class="forum__opportunities">
           <div class="forum__opportunity">
             <div class="forum__text">
               <span class="forum__text_green">Crypto Юг 2023</span> - прекрасная возможность провести время приятно и с пользой, открыть для себя новые возможности, повысить экспертность и увеличить
@@ -197,12 +198,209 @@ export default function () {
               проектами и инициативами, которые могут оказать значительное влияние на индустр ию.
             </div>
           </div>
-        </div>
+        </section>
         <div class="forum__confidence forum__confidence_margin forum__text">
           Мы уверены, что наш криптофорум предоставит тебе всю ценную информацию, деловые возможности и возможности, которых больше нигде не встретишь.
         </div>
-        <div className="forum__images"></div>
+        <div class="forum__images">
+          <DisplayImages
+            items={images?.map((item: any) => {
+              return (
+                <div class="gallery_slide">
+                  <img
+                    ref="slide"
+                    class="forum__image"
+                    src={`/contents/forum/images/${item?.image}`}
+                    alt="img"
+                  />
+                </div>
+              );
+            })}
+            buttons={false}
+            dots={false}
+          />
+        </div>
+        <section class="event_section">
+          <h2 class="forum__title forum__title_margin">Спикеры</h2>
+          <div class="events_list">
+            <div class="tabs">
+              {tabs.map((item, index) => {
+                return (
+                  <span
+                    class="tab"
+                    ref="tabItem1"
+                    onclick={() => {
+                      Static.activeIndex = index;
+                      Ref.activeTab.style.left = `${Ref.tabItem1.offsetWidth * Static.activeIndex}px`;
+                      Static.speakersTabName = item.name;
+                      Fn.log("tab", speakers, arrFinish);
+                      Ref.speakersContent.classList.add("animated");
+                      setTimeout(() => {
+                        Ref.speakersContent.classList.remove("animated");
+                      }, 500);
+                    }}
+                  >
+                    {item.name}
+                  </span>
+                );
+              })}
+              <div
+                class="activeTab"
+                ref="activeTab"
+              ></div>
+            </div>
+
+            {/* speakers */}
+
+            <div
+              class="speakers"
+              ref="speakersContent"
+            >
+              {speakers
+                .filter((item) => item.visited.includes(Static.speakersTabName))
+                .slice(0, Static.howMutchSpeakers)
+                .map((item, index) => {
+                  return (
+                    <div class={["speaker"]}>
+                      <div class="speaker__photo">
+                        <img
+                          src={`/contents/forum/speakers/${item.photo}`}
+                          alt="img"
+                        />
+                      </div>
+                      <div class="speaker__about">
+                        <h3>{item.speaker}</h3>
+                        <p>{item.about_speaker}</p>
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
+
+          {/* speakers show_all button */}
+
+          <div
+            class="show-all"
+            ref="button"
+            onclick={() => {
+              if (Static.howMutchSpeakers == 6) {
+                Static.howMutchSpeakers = speakers.length;
+                Ref.buttonSpan.innerText = "Скрыть";
+              } else {
+                Static.howMutchSpeakers = 6;
+                Ref.buttonSpan.textContent = "Показать всех";
+              }
+              Fn.log("=9b27aa=", speakers);
+            }}
+          >
+            <span
+              ref="buttonSpan"
+              class="show-all__button"
+            >
+              Показать всех
+            </span>
+          </div>
+        </section>
+        <section class="forum__stend">
+          <h2 class="forum__title forum__title_margin">Стенды</h2>
+        </section>
+        <section class="forum__guests">
+          <h2 class="forum__title forum__title_margin">Гости Crypto Emergency</h2>
+          <div class="forum__guests-list">
+            {guests.map((item) => {
+              return (
+                <a
+                  href={item.href}
+                  target="_blank"
+                  class="forum__guest mX-auto"
+                >
+                  <div class="forum__guest-wrap">
+                    <img
+                      src={item.image}
+                      alt={item.name}
+                      class="forum__guest-img"
+                    />
+                  </div>
+
+                  <h4 class="forum__guest-name">{item.name}</h4>
+                  <p class="forum__guest-desc">{item.desc}</p>
+                </a>
+              );
+            })}
+          </div>
+        </section>
       </div>
+
+      <section class="forum__theme">
+        <div class="wrapper wrapper_padding">
+          <p class="forum__theme-text forum__theme-text_light">Второй ежегодный криптовалютный форум пройдет в центре города Новороссийска на берегу Черного моря.</p>
+          <h2 class="forum__title mY-25">Темы форума:</h2>
+          <ul class="forum__theme-list forum__theme-list_margin">
+            <li class="forum__theme-item forum__theme-text">Децентрализованные социальные сети</li>
+            <li class="forum__theme-item forum__theme-text">Как начать свой путь в крипто индустрии</li>
+            <li class="forum__theme-item forum__theme-text">Регуляция и её влияние на рынок</li>
+            <li class="forum__theme-item forum__theme-text">Трейдинг и как на нем заработать</li>
+            <li class="forum__theme-item forum__theme-text">NFT, как прикоснуться к искусству</li>
+            <li class="forum__theme-item forum__theme-text">Майнинг в 2023 году</li>
+            <li class="forum__theme-item forum__theme-text">GameFi получай удовольствие и зарабатывай</li>
+          </ul>
+          <h4 class="forum__theme-title">На форуме Вас будут ждать:</h4>
+          <ul class="forum__theme-list">
+            <li class="forum__theme-item forum__theme-text">30+ проектов</li>
+            <li class="forum__theme-item forum__theme-text">600+ участников</li>
+            <li class="forum__theme-item forum__theme-text">30+ спикеров</li>
+            <li class="forum__theme-item forum__theme-text">Крупные СМИ</li>
+            <li class="forum__theme-item forum__theme-text">Фуршет</li>
+            <li class="forum__theme-item forum__theme-text">Большое количество конкурсов с ценными призами</li>
+          </ul>
+        </div>
+      </section>
+
+      <section class="forum__partners">
+        <div class="wrapper wrapper_padding">
+          <h2 class="forum__title mY-25">Партнеры</h2>
+          <div class="forum__partners-list">
+            {partners?.map((item) => {
+              return (
+                <a
+                  href={item.url}
+                  target="_blank"
+                  class="forum__partners-item"
+                >
+                  <img
+                    src={`/contents/forum/partners/${item?.logo}`}
+                    alt="Партнёр"
+                  />
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+
+      <section class="forum__destination">
+        <h2 class="forum__title">Как добраться?</h2>
+        <div class="forum__map">
+          <a
+            class="dg-widget-link"
+            href="http://2gis.ru/novorossiysk/firm/70000001062874729/center/37.78497576713563,44.712494546339606/zoom/17?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=bigMap"
+          >
+            Посмотреть на карте Новороссийска
+          </a>
+          <div class="dg-widget-link">
+            <a href="http://2gis.ru/novorossiysk/firm/70000001062874729/photos/70000001062874729/center/37.78497576713563,44.712494546339606/zoom/17?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=photos">
+              Фотографии компании
+            </a>
+          </div>
+          <div class="dg-widget-link">
+            <a href="http://2gis.ru/novorossiysk/center/37.784264,44.712441/zoom/17/routeTab/rsType/bus/to/37.784264,44.712441╎Crypto emergency, IT компания?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=route">
+              Найти проезд до Crypto emergency, IT компания
+            </a>
+          </div>
+          <noscript style="color:#c00;font-size:16px;font-weight:bold;">Виджет карты использует JavaScript. Включите его в настройках вашего браузера.</noscript>
+        </div>
+      </section>
     </div>
 
     // <div class="forum effect_lines">
--- frontends/forum/index.tsx
+++ frontends/forum/index.tsx
@@ -1,9 +1,17 @@
 import { Cemjsx, front, Func, Static, Fn, Ref } from "cemjs-all";
 import Navigation from "./navigation";
-import partners from "@json/forumPartners";
+import partners from "@json/forum/forumPartners";
+import { load } from "@2gis/mapgl";
 
 front.listener.finish = () => {
   window.onresize = () => (Ref.activeTab2.style.left = `${Ref.tabItem2.offsetWidth * Static.activeIndex}px`);
+  load().then((mapglAPI) => {
+    const map = new mapglAPI.Map("map", {
+      center: [55.31878, 25.23584],
+      zoom: 13,
+      key: "rudcgu3317",
+    });
+  });
   return;
 };
 
@@ -12,10 +20,10 @@ front.func.test = () => {
 };
 
 front.loader = () => {
-  Static.howMutchSpeakers = 8;
+  Static.howMutchSpeakers = 6;
   Static.speakersTabName = "CryptoЮГ2023";
   Static.partnersTabName = "CryptoЮГ2023";
-  Static.partners = partners.filter((item) => item.visited.includes(Static.partnersTabName));
+  // Static.partners = partners.filter((item) => item.visited.includes(Static.partnersTabName));
   return;
 };
 
