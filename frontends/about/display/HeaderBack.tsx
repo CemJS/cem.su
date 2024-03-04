86527a6fd242e8c90c1162dc13a3e97b87ec8f3e
Konstantin <s4etmc@yandex.ru>
2024-03-04 12:02:20 +0300

    stream

--- frontends/about/display/HeaderBack.tsx
+++ frontends/about/display/HeaderBack.tsx
@@ -3,20 +3,27 @@ import { Cemjsx, Fn, front, Static, Events } from "cemjs-all";
 export default function () {
   // Fn.log('=f24bd3=',Static.titleHead)
   return (
-    <div class="bg-[#1d2029] border-b border-[#2d3243] fixed w-full py-2 z-10 top-0 left-0">
-      <div class="max-w-7xl mx-auto">
-        <div class="flex justify-between items-center">
+    <div class="fixed left-0 top-0 z-10 w-full border-b border-[#2d3243] bg-[#1d2029] py-2 ">
+      <div class="mx-auto max-w-7xl">
+        <div class="flex items-center justify-between">
           <span
-            class="cursor-pointer flex justify-center items-center"
+            class="flex cursor-pointer items-center justify-center"
             onclick={() => Fn.linkChange("/")}
           >
-            <i class="i-arrow-left font-['cemicons'] not-italic font-normal normal-case leading-4 text-[1.3rem] bg-clip-text"></i>
+            <i class="i-arrow-left bg-clip-text font-['cemicons'] text-[1.3rem] font-normal normal-case not-italic leading-4"></i>
           </span>
 
-          <h5 class="back-title">О нас</h5>
+          <h5
+            style="-webkit-line-clamp: 1;
+              -webkit-box-orient: vertical;"
+            class="flex items-center overflow-hidden text-xl"
+          >
+            О нас
+          </h5>
 
           <span
-            class="back-ellipsis"
+            // class="back-ellipsis after:content-['...'] w relative w-[1.925rem] cursor-pointer hover:bg-[#363b4b]"
+            class="back-ellipsis relative w-[1.925rem] cursor-pointer after:absolute after:left-0 after:top-0 after:translate-x-[-10%] after:translate-y-[-70%] after:text-[3rem] after:content-['...'] hover:after:text-[#363b4b]"
             onclick={() => Fn.initOne("modalTools", {})}
           ></span>
         </div>
