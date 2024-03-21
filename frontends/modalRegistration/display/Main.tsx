7f707ca7e52249cdac63bac2e8647ab80f167acc
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2024-03-21 18:03:31 +0300

    modal registration work

--- frontends/modalRegistration/display/Main.tsx
+++ frontends/modalRegistration/display/Main.tsx
@@ -26,12 +26,34 @@ const RenderSteps = function ({ steps, current }) {
 
 const Step1 = function () {
   return (
-    <div class="modalReg_page" ref="slidePage">
-      <div class="f-col">
-        <h3 class="modalReg_page-title">Подтвердите адрес электронной почты</h3>
-        <div
+    <div class="w-1/4 transition-all" ref="slidePage">
+      <div class="flex flex-col">
+        <h3 class="font-semibold text-lg max-@600:text-base mb-4">Подтвердите адрес электронной почты</h3>
+
+        <label for="input-group-1" class="block mb-2 text-sm font-medium text-white">Email</label>
+        <div class="relative mb-6">
+          <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+              <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
+              <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
+            </svg>
+          </div>
+          <input
+            type="text"
+            id="input-group-1"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="name@flowbite.com"
+            oninput={(e: any) => {
+              Static.form.email.value = e.target.value;
+              front.Services.functions.formEmail(Static.form.email);
+            }}
+          />
+        </div>
+
+
+        {/* <div
           class={[
-            "modalWindow_field",
+            "w-full relative h-12 leading-10",
             Static.form.email.value.length ? "modalWindow_field__valid" : null,
             Static.form.email.error ? "modalWindow_field__error" : null,
             Static.form.email.valid ? "modalWindow_field__success" : null,
@@ -39,6 +61,7 @@ const Step1 = function () {
           ]}
         >
           <input
+            class="h-12 absolute w-full outline-none text-base px-7 rounded-2xl border-solid border-[#5f479b] border-[1px] bg-[transparent] transition-all z-[3px] text-white"
             type="email"
             required
             autocomplete="off"
@@ -48,7 +71,7 @@ const Step1 = function () {
               Func.checkForm();
             }}
           />
-          <div class="modalWindow_field_labelLine">
+          <div class="absolute text-base px-2 mx-5 bg-[#202432] transition-all flex items-center gap-2">
             <i class="i i-user"></i>
             <span>{Static.form.email.placeholder}</span>
           </div>
@@ -66,7 +89,7 @@ const Step1 = function () {
               <i class="i i-edit"></i>
             </span>
           ) : null}
-        </div>
+        </div> */}
       </div>
 
       {this.Static.waitCode ? (
@@ -172,7 +195,7 @@ const Step1 = function () {
 
 const Step2 = function () {
   return (
-    <div class="modalReg_page">
+    <div class="w-1/4 transition-all">
       <div class="modalReg_form">
         <div class="f-col">
           <h3 class="modalReg_page-title">Заполните информация о себе</h3>
@@ -358,7 +381,7 @@ const Step2 = function () {
 
 const Step3 = function () {
   return (
-    <div class="modalReg_page">
+    <div class="w-1/4 transition-all">
       <div class="modalReg_form">
         <h3 class="modalReg_page-title">Придумайте пароль</h3>
         <div
@@ -523,7 +546,7 @@ const Step3 = function () {
 
 const Step4 = function () {
   return (
-    <div class="modalReg_page">
+    <div class="w-1/4 transition-all">
       <div class="modalReg_form">
         <h3 class="modalReg_page-title text-center">
           Поздравляем, Вы успешно зарегистрированы!
