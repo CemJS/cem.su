a65a5845d5bcc58e39249f5751bcd6f1908bcfa8
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2024-03-21 14:56:14 +0300

    working modal registration

--- frontends/modalRegistration/display/Header.tsx
+++ frontends/modalRegistration/display/Header.tsx
@@ -2,8 +2,8 @@ import { Cemjsx, Static, Func, Ref } from "cemjs-all"
 
 export default function () {
   return (
-    <header class="modal-header">
-      <h2 class="modal-header__title">Регистрация</h2>
+    <header class="flex justify-between items-center mb-5">
+      <h2 class="text-2xl max-@464:text-lg font-bold">Регистрация</h2>
       <button class="btn btn_dark" onclick={Func.close}>
         <i class="i i-cancel"></i>
       </button>
--- frontends/modalRegistration/navigation.tsx
+++ frontends/modalRegistration/navigation.tsx
@@ -5,7 +5,7 @@ import Main from "./display/Main"
 export default function () {
   return (
     <div class="min-h-full flex items-center justify-center p-6" ref="modalBody">
-      <div class="rounded-3xl max-w-96 w-full relative p-6 text-white bg-[#202432]">
+      <div class="rounded-3xl max-w-3xl w-full relative p-6 text-white bg-[#202432]">
         <Header />
         <Main />
       </div>
