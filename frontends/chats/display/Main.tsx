eb6fe34a852c7ca8457f4bb49a30ccc721305f16
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2024-04-15 15:31:36 +0300

    add url for chats

--- frontends/chats/display/Main.tsx
+++ frontends/chats/display/Main.tsx
@@ -2,6 +2,21 @@ import { Cemjsx } from "cemjs-all"
 
 export default function () {
     return (
-        <div>Chats</div>
+        // start (maybe use tag section)
+        <div class="chat-section">
+            <div class="chat-container">
+                {/* sidebat start */}
+                <div class="chats-sidebar">
+                    {/* вернуться назад расположи перед списком переписок */}
+                    <ul>
+                        <li>
+
+                        </li>
+                    </ul>
+                </div>
+                {/* sidebar end */}
+            </div>
+        </div>
+        // end
     )
 }
